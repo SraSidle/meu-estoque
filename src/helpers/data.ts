68ca4c90110e242e6486d4c4a55b5de290a2b598
Bianca Aguiar <biancaaguiar2021@gmail.com>
2023-03-18 22:40:19 -0300

    feat: mock data configuration

---
+++ src/helpers/data.ts
@@ -0,0 +1,36 @@
+import tornozeleira from "assets/images/tornozeleira-basic.jpg";
+import luva from "assets/images/luva-futebol-de-campo.jpg";
+import bag from "assets/images/bag-transversal-multiesporte.jpg";
+import bola from "assets/images/bola-futebol-de-campo.jpg";
+
+
+export const mockProducts  = [
+    {
+        id: 1001,
+        name: "Bag transversal Multiesporte",
+        image: bag,
+        price: "199,90",
+        department: "Esportes e Lazer"
+    },
+    {
+        id: 1002,
+        name: "Bola Futebol de Campo Extra 32 Gomos Onys",
+        image: bola,
+        price: "199,90",
+        department: "Esportes e Lazer"
+    },
+    {
+        id: 1003,
+        name: "Luva Futebol de Campo Profissional Champion 7",
+        image: luva,
+        price: "199,90",
+        department: "Esportes e Lazer"
+    },
+    {
+        id: 1004,
+        name: "Tornozeleira Basic",
+        image: tornozeleira,
+        price: "199,90",
+        department: "Esportes e Lazer"
+    }
+]
